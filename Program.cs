3ba3e9b9dbade4d290959dc71ed5b4aad15cf1e0
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-27 16:18:24 +0700

    add method input

--- Program.cs
+++ Program.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace Activity3
 {
-    internal class Program
+    class Program
     {
+        //array to be searched
+        int[] arr = new int[20];
+        //number of element in the array
+        int n;
+        //get the number of element to store in the array
+        int i;
+
+        public void input()
+        {
+            while(true)
+            {
+                Console.Write("enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 element.\n");
+            }
+        }
         static void Main(string[] args)
         {
         }
