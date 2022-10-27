152445c795b8755c3b96a2ff5483d6682fff0485
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2022-10-27 16:22:24 +0700

    melengkapi method public input

--- Program.cs
+++ Program.cs
@@ -27,6 +27,17 @@ namespace Activity3
                 else
                     Console.WriteLine("\nArray should have minimum 1 and maximum 20 element.\n");
             }
+            //accept array element
+            Console.WriteLine("");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(" Enter array elemenet ");
+            Console.WriteLine("----------------------");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine("<" + (i + 1) + ">");
+                string s1 = Console.ReadLine();
+                arr[i] = int.Parse(s1);
+            }
         }
         static void Main(string[] args)
         {
